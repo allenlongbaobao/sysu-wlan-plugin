45dc592cbae06cd2335e7a4dfadca91a653f7617
allen <allenwangyu0311@gmail.com>
2014-03-03 20:26:07 +0800

    完成popup的探索

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab){
-  chrome.tabs.executeScript(null, {file: "sysuwlan.js"});
   login();
   setInterval(function(){
    	login();
   }, 60*1000);
 });
+
 
---
+++ popup.js
@@ -0,0 +1,12 @@
+var start = document.getElementById("start");
+var close = document.getElementById("close");
+var option= document.getElementById("option");
+start.onclick = function(){
+  login();
+}
+close.onclick = function(){
+  logout();
+}
+option.onclick = function(){
+  
+}
